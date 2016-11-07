c181fd621bb248edb2036af5b51b18503ecde14e
sajol <sajolkk@gmail.com>
2016-11-07 19:59:29 +0600

    configuration using application.properties file

--- src/main/java/com/springboot/controllers/HelloController.java
+++ src/main/java/com/springboot/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package com.springboot.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HelloController {
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello(ModelMap model) {
+        logger.info("Inside " + this.getClass().getName());
+        logger.debug("Inside " + this.getClass().getName());
         model.addAttribute("greetings", "Hello!!!!");
         return "hello";
     }
